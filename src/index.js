a719281379139fda4310a624c53492802a7584f1
ngandung <gandungn@gmail.com>
2025-03-26 14:22:31 +0700

    declaration complete props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+function Test() {
+  const [rating, setRating] = useState(0);
+ return <div>
+  <StarRating maxStar={5} message={["Bad","Okay","Good","Great","Perfect"]} onSetRating={setRating} />
+  <p>Rating saat ini adalah {rating}</p>
+ </div>
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Test />
+    <StarRating maxStar={5} color="blue" />
   </React.StrictMode>
 );
 
